618e75f38a90ef65753f34d1a5a412173700bd64
chenghongyan2025 <chenghongyan2023@163.com>
2025-11-05 20:49:29 +0800

    feat: 删除不再使用的数据库和工具文件，更新.gitignore以忽略相关目录

--- API/utils/Video2TextUtils.py
+++
@@ -1,6 +0,0 @@
-from funasr import AutoModel
-
-def init_auto_model(model_name):
-    return AutoModel(model=model_name,  vad_model="fsmn-vad",  punc_model="ct-punc", 
-                    #   spk_model="cam++", 
-                      )
